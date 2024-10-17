14191d5f5e110fbe75ab980172f8dfcd115cea9b
andempsey8 <andempsey8@gmail.com>
2024-10-17 17:49:00 -0400

    created game class with initialization and handscore functions

--- game.py
+++ game.py
@@ -1,18 +1,22 @@
 import random
+#have dealer hand and score built into game, game initialization accepts an instance of the ai agent as the opposing player
 
-#Creates deck
-suits = ["Spades","Clubs","Diamonds","Hearts"]
-values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
-fourDeck = [(value,suit) for suit in suits for value in values for decks in range[0:3]]
+class Game:
+    def __init__(self, agent):
+        suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
+        values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+        self.fourDeck = [(value, suit) for suit in suits for value in values for deck in range[0:3]]
+        random.shuffle(self.fourDeck)
 
-#Calculates current hands value
-def handScore(card, currentScore):
-    if card[0] in ['Jack', 'Queen', 'King']:
-        return 10
-    elif card[0] == 'Ace' and currentScore + 11 < 21:
-        return 11
-    elif card[0] == 'Ace' and currentScore + 11 > 21:
-        return 1
-    else:
-        return int(card[0])
-
+    def handScore(self, cards, currentScore):
+        score = 0
+        for card in cards:
+            if card[0] in ['Jack', 'Queen', 'King']:
+                score += 10
+            elif card[0] == 'Ace' and currentScore + 11 < 21:
+                score += 11
+            elif card[0] == 'Ace' and currentScore + 11 > 21:
+                score += 1
+            else:
+                score += int(card[0])
+        return score
