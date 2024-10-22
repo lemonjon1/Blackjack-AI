233c392ee981ba6b361b27f8ca43b91cd1071bf9
Winzhu <winsonzhu15@gmail.com>
2024-10-22 13:56:28 -0400

    Game creates game enviroment, dealer, and player

--- game.py
+++ game.py
@@ -1,18 +1,54 @@
 import random
+#Have dealer hand and score built into game, game initialization accepts an instance of the AI agent as the opposing player
 
-#Creates deck
-suits = ["Spades","Clubs","Diamonds","Hearts"]
-values = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
-fourDeck = [(value,suit) for suit in suits for value in values for decks in range[0:3]]
+suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
+values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
-#Calculates current hands value
-def handScore(card, currentScore):
-    if card[0] in ['Jack', 'Queen', 'King']:
-        return 10
-    elif card[0] == 'Ace' and currentScore + 11 < 21:
-        return 11
-    elif card[0] == 'Ace' and currentScore + 11 > 21:
-        return 1
-    else:
-        return int(card[0])
+class Game:
+    def __init__(self, agent):
+        self.gameDeck = []
+
+        self.gameDeck = [(value, suit) for suit in suits for value in values for deck in range[0:3]]
+        random.shuffle(self.gameDeck)
+
+    class Dealer:
+        def __init__(self):
+            self.currentScore = 0
+            self.hand = []
+
+    class Player:
+        def __init__(self):
+            self.currentScore = 0
+            self.hand = []
+
+    def handScore(self, cards, currentScore):
+        score = 0
+        for card in cards:
+            if card[0] in ['Jack', 'Queen', 'King']:
+                score += 10
+            elif card[0] == 'Ace' and currentScore + 11 < 21:
+                score += 11
+            elif card[0] == 'Ace' and currentScore + 11 > 21:
+                score += 1
+            else:
+                score += int(card[0])
+        return score
+
+    def dealHand(self):
+        hand = []
+        for i in range(2):
+            card = self.gameDeck.pop()
+        return hand
+
+    def hit(self, hand):
+        hand.append(self.gameDeck.pop())
+
+def playGame(self):
+    game = Game()
+
+if __name__ == "__main__":
+    playGame()
+
+
+
 
