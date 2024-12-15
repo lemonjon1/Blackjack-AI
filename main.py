b16f7a4f35fa8212c6a608c3633406901ceabdf4
Nate De Lucia <natedelucia@gmail.com>
2024-12-15 18:43:35 -0500

    Final commit hopefully. Graphs and shi

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def create_blackjack():
     suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
     values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
     deck = [(value, suit) for suit in suits for value in values]
-    agent = game.Player()
     newGame = game.Game()
     return Environment(deck, newGame)
 
@@ -20,56 +19,62 @@ gym.register(
 
 env = gym.make("gymnasium_env/CSE368-BlackJack-v0")
 
+# RL Agent
+
 agent = "A2C"
 policy = "MultiInputPolicy"
-timesteps = 10_000_000
+timesteps = 10_000
 
 model = A2C(policy, env, verbose=1)
 model.learn(total_timesteps=timesteps)
-# model.save(f"{agent}_{policy}")
 del model
 
 average = []
 sum = 0
 for i in range(len(game.rewardList)):
     sum += game.rewardList[i]
-    if i%100000 == 0:
-        average.append(sum/100000)
+    if i%(timesteps/100) == 0:
+        average.append(sum/(timesteps/100))
         sum = 0
 
+# RL Agent Plotting
+
 plt.plot(list(range(len(average))), average, label="RL Agent")
-# data = np.array(average)
-# data = data[~np.isnan(data)]
-# data = data[~np.isinf(data)]
-# z = np.polyfit(list(range(len(data))), data, 1)
-# p = np.poly1d(z)
-# plt.plot(list(range(len(average))), p(list(range(len(average)))), label="trend1")
+data = np.array(average)
+data = data[~np.isnan(data)]
+data = data[~np.isinf(data)]
+z = np.polyfit(list(range(len(data))), data, 1)
+p = np.poly1d(z)
+plt.plot(list(range(len(average))), p(list(range(len(average)))), label="RL Linear Trend")
+
+# Random Agent
 
 game.rewardList = []
 
-for i in range(int(timesteps * (5/6))):
+for i in range(int(timesteps * (4/6))):
     observation, info = env.reset()
     done = False
     while not done:
         action = env.action_space.sample()
         observation, reward, done, truncated, info = env.step(action)
-    # print (f"Action: {action}, Reward: {reward}, Done: {done}")
 
 average = []
 sum = 0
 for i in range(len(game.rewardList)):
     sum += game.rewardList[i]
-    if i%100000 == 0:
-        average.append(sum/100000)
+    if i%(timesteps/100) == 0:
+        average.append(sum/(timesteps/100))
         sum = 0
+
+# Random Agent Plotting
 
 plt.plot(list(range(len(average))), average, label="Random")
 
-# z = np.polyfit(list(range(len(average))), average, 1)
-# p = np.poly1d(z)
-# plt.plot(list(range(len(average))), p(list(range(len(average)))), label="trend2")
+z = np.polyfit(list(range(len(average))), average, 1)
+p = np.poly1d(z)
+plt.plot(list(range(len(average))), p(list(range(len(average)))), label="Random Linear Trend")
 
-plt.xlabel("Time steps * 100000")
+plt.xlabel(f"Time steps * {timesteps/100}")
 plt.ylabel("Average reward")
 plt.title("RL Agent vs Random Selection")
 plt.legend()
